ab822e5a579e498b022ec26078241086a7cb994b
Ildikó Tóth <ildiko@ildikototh.com>
2018-06-02 21:02:25 -0700

    homepage complete, start of grid structure written. Add JS file for sticky header (not working yet). put footer css in separate file. reorganized css.

---
+++ js/scrollingHeader.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  let navLinks = $('desktopNav li a'),
+      navH = $('header').height(),
+      section = $('section'),
+      documentEl = $(document);
+
+  documentEl.on('scroll', function(){
+    let currentScrollPos = documentEl.scrollTop();
+      section.each(function() {
+      let self = $(this);
+      if (self.offset().top <
+            (currentScrollPos + navH) &&
+            (currentScrollPos + navH) <
+            (self.offset().top + self.outerHeight()) ) {
+               targetClass = '.' + self.attr('class') + '-link';
+               navLinks.removeClass('active');
+               $(targetClass).addClass('active');
+             }
+      });
+
+  });
+})();
+
+// $(".mobile").click(function () {
+//   $("li").toggleClass("visible");
+// });
+//
+// $("li").click(function () {
+//   $("li").toggleClass("active");
+// })
