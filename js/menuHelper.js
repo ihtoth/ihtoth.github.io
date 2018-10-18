c2c624262dcae6dae3129830acf4821a68b91f25
Ildikó Tóth <ildiko@ildikototh.com>
2018-10-18 10:31:38 -0700

    add header-hide when scrolling down, reappear when scrolling up. Create projects section tiles reseponsive behavior and layout. Clean up hero css and make better responsive

--- js/menuHelper.js
+++ js/menuHelper.js
@@ -1,7 +1,24 @@
 
 $(document).ready(function(){
+
+  var prevScrollpos = window.pageYOffset;
+  // var header = ;
+  var $header = $(document.getElementById("header"));
+  var headerHeight = $header.height();
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      header.style.top = "0";
+    } else {
+      header.style.top = "-".concat(headerHeight,"px");
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
+
   $('#topMenu li').click(function(){
     $('#menuToggle input')[0].checked = false;
     // console.log($('#menuToggle input')[0].checked);
   });
+
 });
--- js/scrollingHeader.js
+++ js/scrollingHeader.js
@@ -51,7 +51,7 @@ function smoothScroll(){
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: ((target.offset().top)-80)
+          scrollTop: ((target.offset().top)-120)
         }, 1000,function() {
           // Callback after animation
           // Must change focus!
