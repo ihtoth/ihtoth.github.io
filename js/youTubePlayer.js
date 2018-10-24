10d8520e0f52490354c2cdd97cad4f48c26e3ff0
Ildikó Tóth <ildiko@ildikototh.com>
2018-10-23 22:09:58 -0700

    add overrides css stylesheet add copilot html page, started coding it.

---
+++ js/youTubePlayer.js
@@ -0,0 +1,24 @@
+function onYouTubeIframeAPIReady() {
+  var player;
+  player = new YT.Player('muteYouTubeVideoPlayer', {
+    videoId: 'FA6CpOf1bn8', // YouTube Video ID
+    width: 560,               // Player width (in px)
+    height: 316,              // Player height (in px)
+    playerVars: {
+      autoplay: 0,        // Auto-play the video on load
+      controls: 1,        // Show pause/play buttons in player
+      showinfo: 0,        // Hide the video title
+      modestbranding: 1,  // Hide the Youtube Logo
+      loop: 0,            // Run the video in a loop
+      fs: 1,              // Hide the full screen button
+      cc_load_policy: 0, // Hide closed captions
+      iv_load_policy: 3,  // Hide the Video Annotations
+      autohide: 0         // Hide video controls when playing
+    },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+ }
