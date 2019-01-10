558854be5a5f9cd0ed9330b0c4d2335b49d37102
Ildikó Tóth <ildiko@ildikototh.com>
2019-01-10 15:56:45 -0800

    Start Intel Case Study. Fix class "scrollCardCaption" from "crollCardCaption" and remove max-width on .scrollCardCaption>*:first-child. Add images related to intel case study. Add youtube players in js. Adjust h2 and h1 font-sizes. (reduced h2 to 2.6 on mobile sizes

--- js/youTubePlayer.js
+++ js/youTubePlayer.js
@@ -45,6 +45,53 @@ function onYouTubeIframeAPIReady() {
       }
     }
   });
+
+
+  var playerThree;
+  playerThree = new YT.Player('muteYouTubeVideoPlayerPinDrop', {
+    videoId: 'oa8YTs_5REI', // YouTube Video ID
+    width: 724,               // Player width (in px)
+    height: 487,              // Player height (in px)
+    playerVars: {
+      autoplay: 0,        // Auto-play the video on load
+      controls: 1,        // Show pause/play buttons in player
+      showinfo: 0,        // Hide the video title
+      modestbranding: 1,  // Hide the Youtube Logo
+      loop: 0,            // Run the video in a loop
+      fs: 1,              // Hide the full screen button
+      cc_load_policy: 0, // Hide closed captions
+      iv_load_policy: 3,  // Hide the Video Annotations
+      autohide: 0         // Hide video controls when playing
+    },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+
+  var playerFour;
+  playerFour = new YT.Player('muteYouTubeVideoPlayerCompanySearch', {
+    videoId: 'xMBexbMU7rs', // YouTube Video ID
+    width: 724,               // Player width (in px)
+    height: 487,              // Player height (in px)
+    playerVars: {
+      autoplay: 0,        // Auto-play the video on load
+      controls: 1,        // Show pause/play buttons in player
+      showinfo: 0,        // Hide the video title
+      modestbranding: 1,  // Hide the Youtube Logo
+      loop: 0,            // Run the video in a loop
+      fs: 1,              // Hide the full screen button
+      cc_load_policy: 0, // Hide closed captions
+      iv_load_policy: 3,  // Hide the Video Annotations
+      autohide: 0         // Hide video controls when playing
+    },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
   // $(window).scroll(function() {
   //       $(".YTVideo").each( function() {
   //           if( $(window).scrollTop() > $(this).offset().top - 500) {
