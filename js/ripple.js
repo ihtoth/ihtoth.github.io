ca218e3fb48b05c33d11c53af0651df931e2ba3a
Ildikó Tóth <ildiko@ildikototh.com>
2019-01-31 19:21:39 -0800

    Add ripple javascript and css to index.html. Add project type to card, change opacity transition (hover = show text). removed min-height on .card p

---
+++ js/ripple.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+
+
+    $('.ripple').on('click', function (event) {
+      event.preventDefault();
+
+      var $div = $('<a>'),
+          btnOffset = $(this).offset(),
+      		xPos = event.pageX - btnOffset.left,
+      		yPos = event.pageY - btnOffset.top;
+          // href = $(this).attr('href');
+      var $href = this.href;
+
+
+      $div.addClass('ripple-effect');
+      var $ripple = $(".ripple-effect");
+
+      $ripple.css("height", $(this).height());
+      $ripple.css("width", $(this).width());
+      $div
+        .css({
+          top: yPos - ($ripple.height()/2),
+          left: xPos - ($ripple.width()/2),
+          background: $(this).data("ripple-color")
+        })
+        .appendTo($(this));
+
+
+      window.setTimeout(function(){
+        $div.remove();
+        window.location = $href;
+      }, 300, $href);
+
+
+
+    });
+
+
+  });
