6b6f1e0a474ea986b9d8f681a549cb7007896024
Ildikó Tóth <ildiko@ildikototh.com>
2020-05-28 13:23:04 -0700

    Added logic to FeedEk.js to only pick the post specified via the options passed through. Or allow view all if option is not set.

--- js/FeedEk.js
+++ js/FeedEk.js
@@ -11,15 +11,16 @@
 			DescCharacterLimit: 0,
 			TitleLinkTarget: "_blank",
 			DateFormat: "",
-			DateFormatLang: "en"
+			DateFormatLang: "en",
+      PostNum:0
 		}, opt);
 
-		var id = $(this).attr("id"), s = "", dt;
+		var id = $(this).attr("id"), s = "", dt, postOrder=0;
 		$("#" + id).empty();
 		if (def.FeedUrl == undefined) return;
 		$("#" + id).append('<img src="loader.gif" />');
 		$.ajax({
-			url: "https://feed.jquery-plugins.net/load?url=" + encodeURIComponent(def.FeedUrl) + "&maxCount=" + def.MaxCount + "&dateCulture=" + def.DateFormatLang + "&dateFormat=" + def.DateFormat,
+			url: "https://feed.jquery-plugins.net/load?url=" + encodeURIComponent(def.FeedUrl) + "&maxCount=" + def.MaxCount + "&dateCulture=" + def.DateFormatLang + "&dateFormat=" + def.DateFormat + "&postNum=" + def.PostNum,
 			dataType: "json",
 			success: function (result) {
 				$("#" + id).empty();
@@ -27,30 +28,35 @@
 					return;
 
 				$.each(result.data, function (e, itm) {
-					s += '<li><div class="itemTitle"><a href="' + itm.link + '" target="' + def.TitleLinkTarget + '" >' + itm.title + '</a></div>';
+					++postOrder; //counting posts, this var contains the order of the post in the feed.
+					if(def.PostNum == postOrder || def.PostNum == 0){ //when calling for a specific post, or if none specified (PostNum=0), show max count.
 
-					if (def.ShowPubDate) {
-						dt = new Date(itm.publishDate);
-						s += '<div class="itemDate">';
-						if ($.trim(def.DateFormat).length > 0) {
-							s += itm.publishDateFormatted;
+						s += '<li><div class="itemTitle"><a href="' + itm.link + '" target="' + def.TitleLinkTarget + '" >' + itm.title + '</a></div>';
+
+						if (def.ShowPubDate) {
+							dt = new Date(itm.publishDate);
+							s += '<div class="itemDate">';
+							if ($.trim(def.DateFormat).length > 0) {
+								s += itm.publishDateFormatted;
+							}
+							else {
+								s += dt.toLocaleDateString();
+							}
+							s += '</div>';
 						}
-						else {
-							s += dt.toLocaleDateString();
+						if (def.ShowDesc) {
+							s += '<div class="itemContent">';
+							if (def.DescCharacterLimit > 0 && itm.description.length > def.DescCharacterLimit) {
+								s += itm.description.substring(0, def.DescCharacterLimit) + '...';
+							}
+							else {
+								s += itm.description;
+							}
+							s += '</div>';
 						}
-						s += '</div>';
-					}
-					if (def.ShowDesc) {
-						s += '<div class="itemContent">';
-						if (def.DescCharacterLimit > 0 && itm.description.length > def.DescCharacterLimit) {
-							s += itm.description.substring(0, def.DescCharacterLimit) + '...';
-						}
-						else {
-							s += itm.description;
-						}
-						s += '</div>';
 					}
 				});
+
 
 				$("#" + id).append('<ul class="feedEkList">' + s + '</ul>');
 			}
